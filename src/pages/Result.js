a05a48dda9feb512271d8e1403c77a2179a7b36e
Sinan Karakaya <sinankarakaya1992@gmail.com>
2023-01-26 09:37:44 +0300

    26.01.2023

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -8,21 +8,48 @@ import {
   StatusBar,
   ActivityIndicator,
   Alert,
+  Dimensions,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import styled from 'styled-components';
+import {LineChart} from 'react-native-chart-kit';
+const screenWidth =
+  Dimensions.get('window').width - Dimensions.get('window').width * 0.1;
 
 function Result(props) {
   const [history, setHistory] = useState([]);
-  const [alfabetRatio, setAlfabetRatio] = useState([]);
-  const [mean, setMean] = useState([]);
+  const [alfabetRatioX, setAlfabetRatioX] = useState([]);
+  const [alfabetRatioY, setAlfabetRatioY] = useState([]);
+
+  const [meanX, setMeanX] = useState([]);
+  const [meanY, setMeanY] = useState([]);
 
   useEffect(() => {
+    let _mean = props.route.params.mean;
     let _alfabetRatio = props.route.params.alfabetRatio;
-    let _mean = props.route.params.alfabetRatio;
+    let _alfabetRatioTime = [];
+    let _alfabetRatioTimeValue = [];
+    let _meanTime = [];
+    let _meanValue = [];
 
-    setAlfabetRatio(_alfabetRatio);
-    setMean(_mean);
+    for (var key in _alfabetRatio) {
+      _alfabetRatioTime.push(parseInt(key));
+      _alfabetRatioTimeValue.push(_alfabetRatio[key]);
+    }
+
+    setAlfabetRatioY(_alfabetRatioTimeValue);
+    setAlfabetRatioX(_alfabetRatioTime);
+
+    for (var key in _mean) {
+      _meanTime.push(parseInt(key));
+      _meanValue.push(_mean[key]);
+    }
+
+    setAlfabetRatioY(_alfabetRatioTimeValue);
+    setAlfabetRatioX(_alfabetRatioTime);
+
+    setMeanY(_meanValue);
+    setMeanX(_meanTime);
   }, []);
 
   return (
@@ -51,6 +78,92 @@ function Result(props) {
           </Text>
         </View>
       </ResultSection>
+
+      {alfabetRatioX.length > 0 ? (
+        <View
+          style={{justifyContent: 'center', alignItems: 'center', padding: 40}}>
+          <Text>AlfaBetRatio</Text>
+          <LineChart
+            data={{
+              labels: alfabetRatioX,
+              datasets: [
+                {
+                  data: alfabetRatioY,
+                },
+              ],
+            }}
+            width={Dimensions.get('window').width - 50} // from react-native
+            height={220}
+            yAxisLabel=""
+            yAxisSuffix=""
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+              backgroundColor: '#e26a00',
+              backgroundGradientFrom: '#fb8c00',
+              backgroundGradientTo: '#ffa726',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+              propsForDots: {
+                r: '6',
+                strokeWidth: '2',
+                stroke: '#ffa726',
+              },
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+            }}
+          />
+        </View>
+      ) : null}
+
+      {meanX.length > 0 ? (
+        <View
+          style={{justifyContent: 'center', alignItems: 'center', padding: 40}}>
+          <Text>Mean</Text>
+          <LineChart
+            data={{
+              labels: meanX,
+              datasets: [
+                {
+                  data: meanY,
+                },
+              ],
+            }}
+            width={Dimensions.get('window').width - 50} // from react-native
+            height={220}
+            yAxisLabel=""
+            yAxisSuffix=""
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+              backgroundColor: '#e26a00',
+              backgroundGradientFrom: '#fb8c00',
+              backgroundGradientTo: '#ffa726',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+              propsForDots: {
+                r: '6',
+                strokeWidth: '2',
+                stroke: '#ffa726',
+              },
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+            }}
+          />
+        </View>
+      ) : null}
     </SafeAreaView>
   );
 }
